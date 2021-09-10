a917ad0a0291c376050563b573c98abf3ba78bcc
huynguyen1212 <huyro1212000@gmail.com>
2021-09-11 00:13:37 +0700

    video_call

---
+++ video_call/src/model/Customer.java
@@ -0,0 +1,119 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+//import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+
+@Entity
+//@Indexed
+@Table(name = "customer")
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "idcard")
+    private String idCard;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "tel")
+    private String tel;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "note")
+    private String note;
+
+    public Customer() {
+        super();
+    }
+
+    public Customer(int id, String name, String idCard, String address, String tel, String email, String note) {
+        this.id = id;
+        this.name = name;
+        this.idCard = idCard;
+        this.address = address;
+        this.tel = tel;
+        this.email = email;
+        this.note = note;
+    }
+
+    public Customer(String name, String idCard, String address, String email, String tel, String note) {
+        super();
+        this.name = name;
+        this.idCard = idCard;
+        this.address = address;
+        this.tel = tel;
+        this.email = email;
+        this.note = note;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
